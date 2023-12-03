4e928593dab62e608b8a9e6d7ca625aa8fce3edb
Non Unknown <nonunknown777@gmail.com>
2023-12-03 09:59:15 -0300

    comments

--- HPA.gd
+++ HPA.gd
@@ -14,11 +14,12 @@ class_name Test
 		#print("start")
 var size:int = 512
 var radius:int =1
-@export var do_play:bool:
-	set(value):
-		print("find")
-		find_path(from.global_position, to.global_position, self)
-		
+#@export var do_play:bool:
+	#set(value):
+		#print("find")
+		#
+		#find_path(from.global_position, to.global_position, self)
+		#
 
 func on_path_found(path:PackedVector3Array, success:bool) -> void:
 	get_child(0).draw_path(path)
@@ -31,16 +32,13 @@ func _ready() -> void:
 	set_grid_size(size);
 	set_cell_radius(radius);
 	start()
+	#find path function takes start and end position
+	# the last argument is a Node that must contains the
+	# on_path_found(path:PackedVector3Array, success:bool)
+	# function above, will change to Callable in the future
+	# but has some bugs
 	find_path(from.global_position, to.global_position, self)
-	#get_child(0).draw_path(path)
-	#iterate_grid_cells(test)
-	#print(get_grid_size())
 	set_process(true)
-	#print(path)
-	#get_child(0).positions = get_all_cells_position()
-	#get_child(0).can_generate = true
-	#print("test", test)
-	#get_child(0).do_it()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
